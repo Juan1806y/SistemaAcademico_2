b2d304bc549e76c5865e01fe3b7f0ba5fa03062b
Juan David <juandavidorca@ufps.edu.co>
2025-03-27 10:03:23 -0500

    configuración inicial e implementaciones.

---
+++ src/main/java/co/edu/ufps/backend/model/Persona.java
@@ -0,0 +1,22 @@
+package co.edu.ufps.backend.model;
+import java.util.Date;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Persona {
+    @Id
+    private Long cedula;
+    private String nombre;
+    private Long telefono;
+    private Date fechaNacimiento;
+    private Integer edad;
+    private String correoElectronico;
+    private String correoInstitucional;
+    private String direccion;
+    private String sexo;
+
+}
